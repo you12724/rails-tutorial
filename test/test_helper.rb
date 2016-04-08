effe1afb979f9d95bdf8d58e9b7388686de88b15
you12724 <id-12724.s.v@ruri.waseda.jp>
2016-04-08 08:11:50 +0100

    chapter8 終了

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,27 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-end
+  # テストユーザーがログインしていればtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email:       user.email,
+        password:    password,
+        remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+    private
+    # 統合テスト内ではtrueを返す
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+  end
