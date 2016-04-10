88436d6c67d91613ba93eec91e6f3a7c7f58cba3
you12724 <id-12724.s.v@ruri.waseda.jp>
2016-04-10 05:57:13 +0100

    chapter11 終了

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
+
   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
@@ -9,6 +14,5 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
-
   end
 end
